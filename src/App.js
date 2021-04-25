a2eb2a0dd5cbf5b307430dbbd27e64dd104a90ef
Alejandro Lara <alelaru@gmail.com>
2021-04-25 13:01:48 +0200

    Users can send messages properly and see the read receipt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 		<ChatEngine
       height='100vh'
 			projectID='2619f599-6531-4b7e-8ac9-9942bfca67d8'
-			userName='ale2'
+			userName='alelaru'
 			userSecret='123123'
       renderChatFeed={(chatAppState) => <ChatFeed {...chatAppState} />}
 		/>
